7690ed97df2ad544abe6466edf93f456626c124c
Gaurav Joshi <gauravjoshiaa1@gmail.com>
2024-07-07 11:44:15 +0530

    PasswordGenerator - Added a password generator

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,13 +1,17 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
 
 function App() {
   const [length, setLength] = useState(6)
   const [includeNumbers, setNumbers] = useState(false)
   const [includeCharacters, setCharacters] = useState(false)
   const [password, setPassword] = useState('')
+
+  //useRef hook
+  const passwordRef = useRef(null)
+
   //let password=""
   console.log("render")
-  const generatePassword =useCallback(()=>{
+  const generatePassword =()=>{
       let str='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
       let numbers='0123456789'
       let special='!@#$%^&*-_+=[]{}~`'
@@ -23,7 +27,13 @@ function App() {
       }
       console.log("password : "+pass)
       setPassword(pass)
-  },[])
+  }
+  const copypasswordToclipboard =()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,20)
+    window.navigator.clipboard.writeText(password) //window.navigator.clipboard.writeText( ) ye clipboard me text ko copy karne ke liye use hota hai
+  }
+
  
   //generatePassword(); maine yaha par call laga di jiske vajhe se state change hui or component re-render hua
   // isliye ye infinite loop ban gaya to hum isko yaha par call nahi laga sakte isko hum button triggered bhi bana sakte hai lekin hum chahte hai ki ye khud generate ho jaye.
@@ -31,15 +41,15 @@ function App() {
   useEffect(()=>{ 
     console.log("useEffect call")
     generatePassword()
-  },[length,includeNumbers,includeCharacters,generatePassword])
+  },[length,includeNumbers,includeCharacters])
  
   return (
      <div className='bg-slate-900 w-screen h-screen'>
       <h1 className='text-white text-center p-4'>Password Generator</h1>
       <div className=' flex justify-center'>
         <div className=' w-1/2 flex rounded-lg overflow-hidden m-10 max-w-6xl'>
-          <input type="text" value={password} placeholder='password' className='rounded-lg outline-none w-full py-1 px-3 mr-2' />
-          <button className='bg-blue-600 text-white'>copy</button>
+          <input type="text" value={password} placeholder='password' className='rounded-lg outline-none w-full py-1 px-3 mr-2' ref={passwordRef} />
+          <button className='bg-blue-600 text-white' onClick={copypasswordToclipboard}>copy</button>
         </div>
       </div>
       <div className='flex justify-center h-8 items-center overflow-hidden'>
@@ -75,7 +85,7 @@ function App() {
            />
           <label className='text-green-500'>characters</label>
         </div>
-        
+
       </div>
       
      </div>
