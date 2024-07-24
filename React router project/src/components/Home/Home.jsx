c70ad082a9ac697f20ed4c00938da1a80cb24cca
Gaurav Joshi <gauravjoshiaa1@gmail.com>
2024-07-24 16:16:20 +0530

    completed Todo

--- React router project/src/components/Home/Home.jsx
+++ React router project/src/components/Home/Home.jsx
@@ -1,24 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div className="mx-auto w-screen h-screen"
-            style={{
-                backgroundImage: `url("src/components/Home/pexels-rostislav-5011647.jpg")`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-            }}
-        >
-
+        <div className="mx-auto w-full max-w-7xl">
             <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
-                {/* <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+                <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-                         <h2 className="text-4xl font-bold sm:text-5xl">
+                        <h2 className="text-4xl font-bold sm:text-5xl">
                             Download Now
                             <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
-                        </h2> 
+                        </h2>
 
                         <Link
                             className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
@@ -37,18 +29,18 @@ export default function Home() {
                             &nbsp; Download now
                         </Link>
                     </div>
-                </div> */}
+                </div>
 
-                {/* <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="src\components\Home\pexels-rostislav-5011647.jpg" alt="image1" />
-                </div> */}
+                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                </div>
             </aside>
 
-            {/* <div className="grid  place-items-center sm:mt-20">
+            <div className="grid  place-items-center sm:mt-20">
                 <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
-            </div> */}
+            </div>
 
-            {/* <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1> */}
+            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
         </div>
     );
 }
